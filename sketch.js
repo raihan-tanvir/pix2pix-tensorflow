b3d5509c3d5ab29ef4b3958bd552ecf45ed7f257
Raihan Tanvir <raihantanvir.96@gmail.com>
2020-11-21 12:12:24 +0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,89 @@
+const SIZE = 256;
+let inputImg, inputCanvas, output, statusMsg, pix2pix, clearBtn, transferBtn, currentColor, currentStroke;
+
+function setup() {
+  // Create a canvas
+  inputCanvas = createCanvas(SIZE, SIZE);
+  inputCanvas.class('border-box').parent('input');
+
+  // Display initial input image
+  inputImg = loadImage('images/1.png', drawImage);
+
+  // Selcect output div container
+  output = select('#output');
+  statusMsg = select('#status');
+
+  // Get the buttons
+  currentColor = color(0, 0, 0);
+  currentStroke = 1;
+ 
+  select('#size').mouseReleased(() => currentStroke = select('#size').value());
+
+  // Select 'transfer' button html element
+  transferBtn = select('#transferBtn');
+
+  // Select 'clear' button html element
+  clearBtn = select('#clearBtn');
+  // Attach a mousePressed event to the 'clear' button
+  clearBtn.mousePressed(function() {
+    clearCanvas();
+  });
+
+  // Set stroke to black
+  stroke(0);
+  pixelDensity(1);
+
+  // Create a pix2pix method with a pre-trained model
+  pix2pix = ml5.pix2pix('model/enhanced_v.pict', modelLoaded);
+}
+
+// Draw on the canvas when mouse is pressed
+function draw() {
+  if (mouseIsPressed) {
+    stroke(currentColor);
+    strokeWeight(currentStroke)
+    line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
+
+// A function to be called when the models have loaded
+function modelLoaded() {
+  // Show 'Model Loaded!' message
+  statusMsg.html('Model Loaded!');
+  // Call transfer function after the model is loaded
+  //transfer();
+  // Attach a mousePressed event to the transfer button
+  transferBtn.mousePressed(function() {
+    transfer();
+  });
+}
+
+// Draw the input image to the canvas
+function drawImage() {
+  image(inputImg, 0, 0,SIZE, SIZE);
+}
+
+// Clear the canvas
+function clearCanvas() {
+  background(255);
+}
+
+function transfer() {
+  // Update status message
+  statusMsg.html('Transfering...');
+
+  // Select canvas DOM element
+  const canvasElement = select('canvas').elt;
+
+  // Apply pix2pix transformation
+  pix2pix.transfer(canvasElement, function(err, result) {
+    if (err) {
+      console.log(err);
+    }
+    if (result && result.src) {
+      statusMsg.html('Done!');
+      // Create an image based result
+      output.elt.src = result.src;
+    }
+  });
+}
