1458abde9fbee7f86dc90aa4588f6318dddaa4f6
Raihan Tanvir <raihantanvir.96@gmail.com>
2020-11-21 16:17:51 +0600

    Canvas modified

--- app.js
+++ app.js
@@ -7,51 +7,44 @@ function setup() {
   inputCanvas = createCanvas(SIZE, SIZE);
   inputCanvas.class('border-box').parent('input');
 
-  // Set background color
-//   background(255, 0, 0);
+  inputImg = loadImage('images/input_1.png', drawImage);
 
-  // Display initial input image
-  inputImg = loadImage('images/seg0.png', drawImage);
-
-  // Selcect output div container
   output = select('#output');
   statusMsg = select('#status');
 
-  // Get the buttons
-  currentColor = color(0, 0, 255);
-  currentStroke = 17;
+  currentColor = color(0,0,0);
+  currentStroke = 1;
   select('#red').mousePressed(() => currentColor = color(255, 0, 0));
   select('#blue').mousePressed(() => currentColor = color(0, 0, 255));
   select('#size').mouseReleased(() => currentStroke = select('#size').value());
 
-  // Select 'transfer' button html element
   transferBtn = select('#transferBtn');
 
-  // Select 'clear' button html element
   clearBtn = select('#clearBtn');
-  // Attach a mousePressed event to the 'clear' button
   clearBtn.mousePressed(function() {
     clearCanvas();
-    background(255, 0, 0);
+    background(255, 255, 255);
+	statusMsg.html('Canvas Cleared!');
   });
 
   randomBtn = select('#randomBtn');
   randomBtn.mousePressed(function() {
-    let src =['images/1.png', 'images/2.png', 'images/3.png', 'images/4.png', 'images/5.png', 'images/6.png', 'images/7.png'];
-    let index = int(random(0, 8));
+    let src =['images/input_1.png', 'images/input_2.png', 'images/input_3.png', 'images/input_4.png', 'images/input_5.png',
+	'images/input_6.png','images/input_7.png','images/input_8.png','images/input_9.png','images/input_10.png',
+	'images/input_11.png','images/input_12.png','images/input_13.png','images/input_14.png','images/input_15.png',
+	'images/input_16.png','images/input_17.png','images/input_18.png','images/input_19.png','images/input_20.png'];
+    let index = int(random(0, 20));
     inputImg = loadImage(src[index], drawImage);
+	
+	statusMsg.html('Random Sketch Selected');
   });
 
-
-  // Set stroke to black
   stroke(0);
   pixelDensity(1);
 
-  // Create a pix2pix method with a pre-trained model
   pix2pix = ml5.pix2pix('model/enhanced_v.pict', modelLoaded);
 }
 
-// Draw on the canvas when mouse is pressed
 function draw() {
   if (mouseIsPressed) {
     stroke(currentColor);
@@ -60,48 +53,32 @@ function draw() {
   }
 }
 
-// Call transfer function when mouse is released
-// function mouseReleased() {
-//     transfer();
-// }
-
-// A function to be called when the models have loaded
 function modelLoaded() {
-  // Show 'Model Loaded!' message
   statusMsg.html('Model Loaded!');
-  // Call transfer function after the model is loaded
-  //transfer();
-  // Attach a mousePressed event to the transfer button
   transferBtn.mousePressed(function() {
     transfer();
   });
 }
 
-// Draw the input image to the canvas
 function drawImage() {
   image(inputImg, 0, 0,SIZE, SIZE);
 }
 
-// Clear the canvas
 function clearCanvas() {
   background(255);
 }
 
 function transfer() {
-  // Update status message
-  statusMsg.html('Transfering...');
+	statusMsg.html('Generating Jamdani Motif........!');
+	const canvasElement = select('canvas').elt;
 
-  // Select canvas DOM element
-  const canvasElement = select('canvas').elt;
+	pix2pix.transfer(canvasElement, function(err, result) {
 
-  // Apply pix2pix transformation
-  pix2pix.transfer(canvasElement, function(err, result) {
     if (err) {
       console.log(err);
     }
     if (result && result.src) {
       statusMsg.html('Done!');
-      // Create an image based result
       output.elt.src = result.src;
     }
   });
